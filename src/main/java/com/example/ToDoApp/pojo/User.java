7faebb9f8b9ae2c31479ff58f158f301a0eb52a3
Jordi Stark <Jordybecerril@gmail.com>
2021-07-17 18:00:45 -0500

    hmm

---
+++ src/main/java/com/example/ToDoApp/pojo/User.java
@@ -0,0 +1,71 @@
+package com.example.ToDoApp.pojo;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="users")
+public class User {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name="user_id")
+	private int id;
+	
+	@Column(name="username")
+	private String name;
+	
+	@Column(name="password")
+	private String passwordHash;
+	
+	@Column(name="enabled")
+	private boolean enabled;
+	
+	@Column(name="email")
+	private String email;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getPasswordHash() {
+		return passwordHash;
+	}
+
+	public void setPasswordHash(String passwordHash) {
+		this.passwordHash = passwordHash;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public boolean isEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
